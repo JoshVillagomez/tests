d3d8ed2462630f5af3c8c5ba117476f24aee7d7d
JoshVillagomez <eljelix99@gmail.com>
2024-05-09 19:26:17 -0600

    Added POM Objects for Login

--- saucedemo3.spec.ts
+++ saucedemo3.spec.ts
@@ -1,12 +1,20 @@
 import { test, expect } from '@playwright/test';
+import { LoginPage } from './pageobjects/LoginPage';
+import { log } from 'console';
 
 test('purchase and item as STANDAR USER', async ({page}) => {
 
     await page.goto('https://www.saucedemo.com');
-    await page.getByRole('textbox', {name: 'Username'}).fill('standard_user');
+   /*await page.getByRole('textbox', {name: 'Username'}).fill('standard_user');
     await page.getByRole('textbox', {name: 'Password'}).fill('secret_sauce');
-    await page.getByRole('button', {name: 'Login'}).click();
+    await page.getByRole('button', {name: 'Login'}).click();*/
     
+    const loginPage = new LoginPage(page);
+    /*await loginPage.fillUsername('standard_user');
+    await loginPage.fillPassword('secret_sauce');
+    await loginPage.clickOnLogin();*/
+
+    await loginPage.loginWithCredentials('standard_user', 'secret_sauce')
 
     const itemsContainer = await page.locator('#inventory_container .inventory_item').all();
 
